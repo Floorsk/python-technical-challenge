5590e32f8fa20f570b773a00b71b04ec5398f4c3
floorsk <davi.galdino08@gmail.com>
2025-05-22 14:39:47 -0300

    First commit

---
+++ script.py
@@ -0,0 +1,12 @@
+""" 2. Cash Minimizer
+Imagine that you are hired by a countrys mint, the entity which prints paper money and
+coins. Your team studies the effectiveness and velocity of money (how quickly it changes
+hands). Your initial task is to write a piece of software which gives a) a set of cash & coin
+denominations, and b) an amount of money to be transferred between two parties, will
+output both the payment & change which minimizes the number of bills & coins which
+need to be exchanged. For example, lets take the US common cash denominations (i.e.,
+[0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 50, 100]) and an amount of $19.96. The payment &
+change which minimizes the number of bills & coins for this exchange is a payment of 20 +
+0.01 and a change of 0.05. """
+
+# First commit for the this beeing at 14:40 
